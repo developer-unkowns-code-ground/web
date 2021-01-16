5c0acbe17ce034ca9fbbeef7521e0dfa72b1466b
Nipe Setiawan Harefa <nipeharefa@gmail.com>
2021-01-16 20:10:33 +0700

    wallet

--- pages/wallet/index.tsx
+++ pages/wallet/index.tsx
@@ -5,6 +5,18 @@ const Wallet = () => {
         <h3 className="text-xl font-bold">Your Wallet</h3>
         <button className="font-bold text-red-600">Log out</button>
       </div>
+
+      <div className="text-center mt-20">
+        <p className="text-2xl font-bold">You Don’t have any wallet</p>
+        <p className="text-sm">Create one to track your expenses</p>
+      </div>
+
+      <div className="mt-20">
+        <button className="w-full bg-gray-100 py-8">
+          <span>+</span>
+          <p>Add Wallet</p>
+        </button>
+      </div>
     </section>
   );
 };
