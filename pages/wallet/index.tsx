54fde669640e60c07e7bdbac44ae1b13d6641a54
Nipe Setiawan Harefa <nipeharefa@gmail.com>
2021-01-16 20:02:31 +0700

    add wallet

---
+++ pages/wallet/index.tsx
@@ -0,0 +1,12 @@
+const Wallet = () => {
+  return (
+    <section className="max-w-1/100 mx-auto pt-4 px-4">
+      <div className="flex justify-between">
+        <h3 className="text-xl font-bold">Your Wallet</h3>
+        <button className="font-bold text-red-600">Log out</button>
+      </div>
+    </section>
+  );
+};
+
+export default Wallet;
